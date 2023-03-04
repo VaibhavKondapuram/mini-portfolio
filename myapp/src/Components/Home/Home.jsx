0fba277bde5bd7675a468798227f3fc92c7d26c0
Vaibhav kondapuram <vaibhavpawanism@gmail.com>
2023-03-04 18:49:01 +0530

    refactor code according to the full screen size

--- myapp/src/Components/Home/Home.jsx
+++ myapp/src/Components/Home/Home.jsx
@@ -14,25 +14,25 @@ const Home = () => {
       bg-gradient-to-bl
       from-green-400 to-red-400 via-purple-300 animate-gradient-x text-white"
       >
-        <h1 className=" text-6xl sm:h-18 md:h-20 ">Megha Maurya</h1>
+        <h1 className=" text-6xl sm:h-18 md:h-20 ">Vaibhav Kondapuram</h1>
         <span className="text-2xl">A Frontend Developer</span>
         <div className="flex text-5xl -mb-32 my-2">
           <a
-            href="https://github.com/meghamaurya"
+            href="https://github.com/VaibhavKondapuram"
             target="_blank"
             rel="noopener noreferrer"
           >
             <BsGithub className="m-2 mx-3 hover:text-neutral-600" />
           </a>
           <a
-            href="https://www.linkedin.com/in/megha-maurya-21369324b/"
+            href="https://www.linkedin.com/in/vaibhav-kondapuram-1506b698/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <GrLinkedin className="m-2 mx-3 hover:text-neutral-600" />
           </a>
           <a
-            href="https://twitter.com/MeghaMaurya58"
+            href="https://twitter.com/VaibhavPawanist"
             target="_blank"
             rel="noopener noreferrer"
           >
